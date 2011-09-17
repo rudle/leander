bfe1c4f38635e9b9fa45a29875d9cd8dafe82a38
Nolan Evans <nolan@squareup.com>
2011-09-17 14:16:49 -0700

    this is better

--- lib/leander/index.rb
+++ lib/leander/index.rb
@@ -1,42 +1,42 @@
-require 'ferret'
-require 'highline/import'
+require 'ferret';
+require 'highline/import';
 
-require 'leander/webpage.rb'
+require 'leander/webpage.rb';
 
-include Ferret
+include Ferret;
 
-class BmIndex
-	attr_accessor :index
+class BmIndex;
+	attr_accessor :index;
 
-	def initialize
-		@index = Index::Index.new(:path => "#{ENV['HOME']}/.leander.idx")
-	end
+	def initialize;
+		@index = Index::Index.new(:path => "#{ENV['HOME']}/.leander.idx");
+	end;
 
-	def store_webpage(wp)
-		@index << {
-			:url => wp.url,
-			:doc => wp.doc
-		}
-	end
+	def store_webpage(wp);
+		@index << {;
+			:url => wp.url,;
+			:doc => wp.doc;
+		};
+	end;
 
-	def leander_remove query
-		@index.search_each(query) do |id, score|
-			agrees = HighLine.agree("Are you sure you want to delete #{@index[id][:url]} Y/n?") { |q| q.default = 'yes'}
-			if agrees then @index.delete(id) end
-		end
-	end
+	def leander_remove query;
+		@index.search_each(query) do |id, score|;
+			agrees = HighLine.agree("Are you sure you want to delete #{@index[id][:url]} Y/n?") { |q| q.default = 'yes'};
+			if agrees then @index.delete(id) end;
+		end;
+	end;
 
-	def search(query)
-		output = []
-		@index.search_each(query) do |id, score|
-			doc = Nokogiri::HTML.parse(@index[id][:doc])
-			tmp = {}
-			tmp[:url] = index[id][:url]
-			tmp[:title] = doc.css('title').inner_text.strip
-			tmp[:doc] = doc
-			output << tmp
-		end
+	def search(query);
+		output = [];
+		@index.search_each(query) do |id, score|;
+			doc = Nokogiri::HTML.parse(@index[id][:doc]);
+			tmp = {};
+			tmp[:url] = index[id][:url];
+			tmp[:title] = doc.css('title').inner_text.strip;
+			tmp[:doc] = doc;
+			output << tmp;
+		end;
 
-		return output
-	end
-end
+		return output;
+	end;
+end;
